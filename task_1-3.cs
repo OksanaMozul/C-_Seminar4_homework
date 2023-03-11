dc11c867af62608d1af9e5af2e5214df6026e019
Oksana <oksana-k83@mail.ru>
2023-03-11 16:44:32 +0300

    добавлены вторая и третья задачаа

--- task_1-3.cs
+++ task_1-3.cs
@@ -3,66 +3,66 @@
 // Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-// intnumberA=ReadInt("Введите число A: ");
-// intnumberB=ReadInt("Введите число B: ");
-// Stepen(numberA, numberB);
+intnumberA=ReadInt("Введите число A: ");
+intnumberB=ReadInt("Введите число B: ");
+Stepen(numberA, numberB);
 
-// //возводим в степень
-// voidStepen(inta, intb)
-// {
-//     intresult=1;
-//     for(inti=1; i<=b; i++)
-//     {
-//         result=result*a;
-//     }
-//     Console.WriteLine(result);
-// }
+//возводим в степень
+voidStepen(inta, intb)
+{
+    intresult=1;
+    for(inti=1; i<=b; i++)
+    {
+        result=result*a;
+    }
+    Console.WriteLine(result);
+}
 
-// intReadInt(stringmessage)
-// {
-//     Console.WriteLine(message);
-//     returnConvert.ToInt32(Console.ReadLine());
-// }
+intReadInt(stringmessage)
+{
+    Console.WriteLine(message);
+    returnConvert.ToInt32(Console.ReadLine());
+}
 
 // Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
-// int number = ReadInt("Введите число: ");
-// int len = NumberLen(number);
-// SumNumbers(number, len);
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
 
 
-// //  ввод
-// int ReadInt(string message)
-// {
-//     Console.Write(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+//  ввод
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-// // подсчет цифр в числе
-// int NumberLen(int a)
-// {
-//     int index = 0;
-//     while (a > 0)
-//     {
-//         a /= 10;
-//         index++;
-//     }
-//     return index;
-// }
+// подсчет цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
 
-// // вывод суммы цифр в числе
-// void SumNumbers(int n, int len)
-// {
-//     int sum = 0;
-//     for (int i = 1; i <= len; i++)
-//     {
-//         sum += n % 10;
-//         n /= 10;
-//     }
-//     Console.WriteLine(sum);
-// }
+// вывод суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
 
 // Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
