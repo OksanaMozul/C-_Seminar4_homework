24a662a7fb3cf66cfc37916210dd0dc53bc4acf5
Oksana <oksana-k83@mail.ru>
2023-03-11 16:25:41 +0300

    первая задача

--- task_1-3.cs
+++ task_1-3.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Урок 4. Функции
+
+// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// intnumberA=ReadInt("Введите число A: ");
+// intnumberB=ReadInt("Введите число B: ");
+// Stepen(numberA, numberB);
+
+// //возводим в степень
+// voidStepen(inta, intb)
+// {
+//     intresult=1;
+//     for(inti=1; i<=b; i++)
+//     {
+//         result=result*a;
+//     }
+//     Console.WriteLine(result);
+// }
+
+// intReadInt(stringmessage)
+// {
+//     Console.WriteLine(message);
+//     returnConvert.ToInt32(Console.ReadLine());
+// }
+
+// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
